e8313267ce961eade1f962d8c7146e728cade7e8
Patrik Bergman <patrik.bergman@infinera.com>
2017-10-02 15:29:49 +0200

    Changed method signatures

--- functions/docker-machine-commands.sh
+++ functions/docker-machine-commands.sh
@@ -2,7 +2,8 @@
 
 #set -o nounset # Treat unset variables as an error
 
-connect_shell_to_machine() {
+# Connect current shell to a docker-machine
+docker_machine_connect() {
     if [ -z "$1" ] ; then
         printf "No machine name supplied\n"
         printf  "connect_shell_to_machine $1\n"
@@ -13,9 +14,13 @@ connect_shell_to_machine() {
     printf  "docker-machine environment set to: $1\n"
 }
 
-disconnect_shell_from_any_machines() {
+# Disconnect current shell from a docker-machine
+docker_machine_disconnect() {
     eval $(docker-machine env -u)
 }
 
+docker_machine_status() {
+    env | grep DOCKER
+}
 
 
